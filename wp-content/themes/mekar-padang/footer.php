b5f39b4832b9343a0101f3200efabcfde4333479
dvlwj <dvlimweijiang@gmail.com>
2020-06-03 19:09:36 +0700

    add homepage mockup

--- wp-content/themes/mekar-padang/footer.php
+++ wp-content/themes/mekar-padang/footer.php
@@ -1,15 +1,47 @@
 <footer>
   <div class="row">
     <div class="col-sm-12 d-flex align-items-center justify-content-center text-center">
-      <div>
-        <img src="<?php echo theme_image('mekar-logo.gif')?>" loading="lazy"/>
-        <p>
-          *Effects may vary on different subjects</br>
-          Effective at eliminating 99.9% germs in less than 30 seconds
-        </p>
-        <p>
-          &copy; 2020 Mekar All Right Reserved
-        </p>
+      <div class="col-xs-12 col-sm-2 align-self-center">
+        <img class="image-full" src="<?php echo theme_image('komsos.png')?>" loading="lazy"/>
+      </div>
+      <div class="col-xs-12 col-sm-10">
+        <div class="row" style="margin-bottom: 5em;">
+          <div class="col align-self-center">
+            <img class="image-full" src="<?php echo theme_image('gema.png')?>" loading="lazy"/>
+          </div>
+          <div class="col align-self-center">
+            <img class="image-full" src="<?php echo theme_image('mekar.png')?>" loading="lazy"/>
+          </div>
+          <div class="col align-self-center">
+            <img class="image-full" src="<?php echo theme_image('boss_fm.png')?>" loading="lazy"/>
+          </div>
+          <div class="col align-self-center">
+            <img class="image-full" src="<?php echo theme_image('rdb.png')?>" loading="lazy"/>
+          </div>
+          <div class="col align-self-center">
+            <img class="image-full" src="<?php echo theme_image('et_terra.png')?>" loading="lazy"/>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-circle fa-stack-2x"></i>
+              <i class="fab fa-youtube fa-stack-1x fa-inverse"></i>
+            </span>
+          </div>
+          <div class="col">
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-circle fa-stack-2x"></i>
+              <i class="fab fa-facebook fa-stack-1x fa-inverse"></i>
+            </span>
+          </div>
+          <div class="col">
+            <span class="fa-stack fa-lg">
+              <i class="fas fa-circle fa-stack-2x"></i>
+              <i class="fab fa-instagram fa-stack-1x fa-inverse"></i>
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- wp-content/themes/mekar-padang/index.php
+++ wp-content/themes/mekar-padang/index.php
@@ -1,1 +1,198 @@
-nothing to do here.
+<?php get_header(); ?>
+
+<div class="container-fluid">
+  <div class="row" >
+    <div class="col-xs-12 col-sm-3" style="padding:0">
+      <div class="card">
+        <div class="card-header">
+          Kategori
+        </div>
+        <ul class="list-group">
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Orang Kudus
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Katekese
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Moral
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Fabel
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            CHC
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Varia
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Pengetahuan Populer
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Sahabat Mekar
+          </li>
+          <li class="list-group-item d-flex justify-content-between align-items-center">
+            Cergam
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-6" style="padding-top: 1em;">
+      <div class="row" style="margin-bottom: 5em;">
+        <div class="col">
+          <h5 class="text-center w-100">Tema</h5>
+          <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="row">
+                  <div class="col">
+                    <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Pertama" alt="Cerita Pertama">
+                  </div>
+                  <div class="col">
+                    <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Kedua" alt="Cerita kedua">
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="row">
+                  <div class="col">
+                    <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Ketiga" alt="Cerita Ketiga">
+                  </div>
+                  <div class="col">
+                    <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Keempat" alt="Cerita Keempat">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+          </div>
+        </div>
+      </div>
+      <div class="row" style="margin-bottom: 5em;">
+        <div class="col">
+          <h5 class="text-center w-100">Moral</h5>
+          <img class="d-block w-100" src="https://via.placeholder.com/300x300.png?text=Cerita+Moral" alt="Cerita Moral">
+          <p class="text-center w-100">Cerita Moral</p>
+        </div>
+        <div class="col">
+          <h5 class="text-center w-100">Varia</h5>
+          <img class="d-block w-100" src="https://via.placeholder.com/300x300.png?text=Cerita+Varia" alt="Cerita Varia">
+          <p class="text-center w-100">Cerita Varia</p>
+        </div>
+      </div>
+      <div class="row" style="margin-bottom: 5em;">
+        <h4 class="w-100 text-center">Cergam</h4>
+        <div class="col">
+          <h5 class="text-center w-100">Sr. Beata</h5>
+          <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Sr.Beata" alt="Cerita Moral">
+          <p class="text-center w-100">Cerita Sr.Beata</p>
+        </div>
+        <div class="col">
+          <h5 class="text-center w-100">Friska</h5>
+          <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Varia">
+          <p class="text-center w-100">Cerita Friska</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-xs-12 col-sm-3" style="padding:1em 0 0 1em">
+      <div class="row" style="margin-bottom:1em">
+        <img src="https://via.placeholder.com/25x25" alt="Gambar Santa">
+        <h5>&nbsp; Ujud Doa bapa Suci Juni 2020</h5>
+      </div>
+      <div class="row">
+        <img class="d-block w-100" src="https://via.placeholder.com/250x250.png?text=Gambar+Doa+bapa+Suci" alt="Cerita Moral">
+      </div>
+      <div class="row">
+        <h5 class="text-center w-100" style="margin-top: 1em;">Friska</h5>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 1
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 2
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 3
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 4
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 5
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 6
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 7
+              </div>
+            </div>
+          </li>
+          <li class="list-group-item">
+            <div class="row">
+              <div class="col-xs-12 col-sm-5">
+                <img class="d-block w-100" src="https://via.placeholder.com/300x200.png?text=Cerita+Friska" alt="Cerita Moral">
+              </div>
+              <div class="col-xs-12 col-sm-7">
+                Friska 8
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
