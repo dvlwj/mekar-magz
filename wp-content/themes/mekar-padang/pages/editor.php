5ac806b1bbf8b43040a97849ac22b0ca0404f7a9
dvlwj <dvlimweijiang@gmail.com>
2020-11-29 22:48:33 +0700

    make editor message image is configureable from CMS ACF

--- wp-content/themes/mekar-padang/pages/editor.php
+++ wp-content/themes/mekar-padang/pages/editor.php
@@ -1,9 +1,14 @@
 <h2 class="segment-title">&nbsp;</h2>
+<?php
+$editor = get_field('editor',21);
+?>
 <div class="row editor-row">
     <div class="col-xs-12">
         <div class="card thumbnail-card small-thumbnail-card">
             <div class="card-header">
-                <img class="image-full shadow-on-hover" src="<?php echo theme_image('redaksi.jpg');?>" alt="Majalah Mekar Padang"/>
+                <img class="image-full shadow-on-hover"
+                     src="<?php echo $editor['image'];?>"
+                     alt="Majalah Mekar Padang"/>
             </div>
         </div>
     </div>
